cd96c64b7974b32f5576f0d7bc28c1f4f7452700
Yaboialip <aliefsd84@gmail.com>
2024-06-10 10:35:01 +0700

    cobaa

--- resources/views/pelanggan/modal/registerPelanggan.blade.php
+++ resources/views/pelanggan/modal/registerPelanggan.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="{{ route('storePelanggan') }}" method="POST">
+                <form action="{{ route('storePelanggan') }}" method="POST"> 
                     @csrf
                     <div class="mb-3 row">
                         <label for="nama" class="col-sm-3 col-form-label">Name <span
@@ -58,7 +58,7 @@
                     </div>
                     <div class="mb-3 row">
                         <label for="date" class="col-sm-3 col-form-label">Date of birth <span
-                                style="color:red;">*</span>
+                                style="color:blue;">*</span>
                         </label>
                         <div class="col-sm-9">
                             <input type="date" class="form-control" id="date" name="date"
