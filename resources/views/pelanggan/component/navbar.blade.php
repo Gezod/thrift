1d4e7b00ae10ad12f67c9e00eab692ebe63020b3
Yaboialip <aliefsd84@gmail.com>
2024-06-10 18:57:12 +0700

    navbar_change

--- resources/views/pelanggan/component/navbar.blade.php
+++ resources/views/pelanggan/component/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #025464;">
-        <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="assets/images/LOGO.png" alt="AtoZ Logo" style="width: 40px; height: 40px; margin-right: 10px;">
-                <span>AtoZ</span>
-            </a>
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <img src="assets/images/LOGO.png" alt="AtoZ Logo" style="width: 40px; height: 40px; margin-right: 10px;">
+            <span style="font-family: 'Montserrat', sans-serif; font-weight: bold; font-size: 1.5rem;">AtoZ</span>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -12,47 +12,44 @@
             <ul class="navbar-nav gap-4">
                 <li class="nav-item">
                     <a class="nav-link {{ Request::path() == '/' ? 'active' : '' }}" aria-current="page"
-                        href="/">Homeee</a>
+                        href="/" style="font-family: 'Montserrat', sans-serif;">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::path() == 'shop' ? 'active' : '' }}" href="/shop">Shop</a>
+                    <a class="nav-link {{ Request::path() == 'shop' ? 'active' : '' }}" href="/shop" style="font-family: 'Montserrat', sans-serif;">Shop</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Request::path() == 'contact' ? 'active' : '' }}" href="/contact">Contact
-                        Us</a>
+                    <a class="nav-link {{ Request::path() == 'contact' ? 'active' : '' }}" href="/contact" style="font-family: 'Montserrat', sans-serif;">Contact Us</a>
                 </li>
                 @auth
-                    <div class="select" tabindex="0" role="button">
-                        <div class="text-links">
-                            <div class="d-flex gap-2 align-items-center">
-                                <img src="{{ asset('storage/user/' . Auth::user()->foto) }}" class="rounded-circle"
-                                    style="width: 50px;" alt="">
-                                <div class="d-flex flex-column text-white">
-                                    <p class="m-0" style="font-weight: 700; font-size:14px;">{{ Auth::user()->name }}
-                                    </p>
-                                    <p class="m-0" style="font-size:12px">{{ Auth::user()->email }}</p>
-                                </div>
+                    <div class="dropdown">
+                        <a class="d-flex align-items-center text-links text-white" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none;">
+                            <img src="{{ asset('storage/user/' . Auth::user()->foto) }}" class="rounded-circle" style="width: 50px; height: 50px;" alt="">
+                            <div class="d-flex flex-column text-white ms-2">
+                                <p class="m-0" style="font-weight: 700; font-size:14px;">{{ Auth::user()->name }}</p>
+                                <p class="m-0" style="font-size:12px">{{ Auth::user()->email }}</p>
                             </div>
-                        </div>
-                        <div class="links-login text-white" id="links-login">
-                            <a href="logout_pelanggan" style="text-decoration: none" role="button"
-                                tabindex="0">Keluar</a>
-                        </div>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                            <li><a class="dropdown-item" href="logout_pelanggan">Logout</a></li>
+                        </ul>
                     </div>
                 @else
                     <li class="nav-item">
                         <button type="button" class="btn btn-success" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal">
-                            Login | Register</button>
+                            data-bs-target="#exampleModal" style="font-family: 'Montserrat', sans-serif;">
+                            Login | Register
+                        </button>
                     </li>
                 @endauth
                 <li class="nav-item">
-                    <div class="notif">
+                    <div class="notif position-relative">
                         <a href="/transaksi" class="fs-5 nav-link {{ Request::path() == 'transaksi' ? 'active' : '' }}">
                             <i class="fa fa-bag-shopping"></i>
                         </a>
                         @if ($count)
-                            <div class="circle">{{ $count }}</div>
+                            <div class="circle position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                {{ $count }}
+                            </div>
                         @endif
                     </div>
                 </li>
@@ -68,6 +65,7 @@
     </div>
 </nav>
 
+
 <script>
     $(".text-links").click(function(e) {
         e.preventDefault();
@@ -79,3 +77,4 @@
         }
     });
 </script>
+
